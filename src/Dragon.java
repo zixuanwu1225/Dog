0aae16bc36a5a54771c2afb3aa77fb4ae9bfad89
unknown <BT_4N2_26@BKTECH.LOCAL>
2022-12-01 12:35:29 -0500

    Initial commit

---
+++ src/Dragon.java
@@ -0,0 +1,34 @@
+public class Dragon {
+    private int health = 100;
+    private int strength = 1;
+    private int level = 1;
+    private int deathPrevention;
+    private int storedDamage;
+    public Dragon(){
+    }
+    public String takeDamage(int damage){
+        health = health-damage;
+        if (health>0){
+            return "Your Dragons health is: "+health;
+        }
+        else
+        {
+            if (deathPrevention == 1) {
+                return "Your Dragon is already Dead!";
+            }
+            deathPrevention++;
+            return "Your Dragon is Dead.";
+        }
+    }
+    public String attack(){
+        int damage = strength*level;
+        strength=strength+1;
+        storedDamage = storedDamage+damage;
+        if (storedDamage>50){
+            storedDamage=storedDamage-50;
+            level++;
+            return "Your dragon is now level "+level;
+        }
+        return "Your dragon deals "+damage+" damage!";
+    }
+}
---
+++ src/StudentRunner.java
@@ -0,0 +1,26 @@
+public class StudentRunner
+{
+    public static void main(String[] args)
+    {
+        Dragon dragon = new Dragon();
+        System.out.println(dragon.takeDamage(25));
+        System.out.println(dragon.takeDamage(25));
+        System.out.println(dragon.takeDamage(101));
+        System.out.println(dragon.takeDamage(101));
+        System.out.println(dragon.takeDamage(101));
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        System.out.println(dragon.attack());
+        
+    }
+}
+
